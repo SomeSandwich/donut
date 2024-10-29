091fc8d14ba6b6eea77f3eaf9306726ba4b335d1
Hieu Nguyen <hieucckha@gmail.com>
2024-10-29 12:39:54 +0700

    Change project name and add link project DNT-2

---
+++ src/Services/Identity/Identity.API/Controllers/IdentityController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.API.Controllers;
+
+/// <summary>
+/// Represents the controller for managing identity related operations.
+/// </summary>
+[ApiController]
+[Route("api/identity")]
+public class IdentityController : ControllerBase
+{
+    private readonly ILogger<IdentityController> logger;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="logger">Logger instance.</param>
+    public IdentityController(ILogger<IdentityController> logger)
+    {
+        this.logger = logger;
+    }
+
+    /// <summary>
+    /// Retrieves the identity information.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The identity response.</returns>
+    [HttpGet("")]
+    public async Task<ActionResult<GetIdentityResponse>> GetIdentity(CancellationToken cancellationToken)
+    {
+        return new GetIdentityResponse
+        {
+            Identity = "Identity.API"
+        };
+    }
+
+    /// <summary>
+    /// Represents the response for the GetIdentity method.
+    /// </summary>
+    public class GetIdentityResponse
+    {
+        /// <summary>
+        /// Gets or sets the identity value.
+        /// </summary>
+        public string Identity { get; set; }
+    }
+}
---
+++ src/Services/Link/Link.API/Controllers/LinkController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Link.API.Controllers;
+
+/// <summary>
+/// Represents a controller for managing links.
+/// </summary>
+[ApiController]
+[Route("api/link")]
+public class LinkController : ControllerBase
+{
+    private readonly ILogger<LinkController> logger;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="logger">The logger instance.</param>
+    public LinkController(ILogger<LinkController> logger)
+    {
+        this.logger = logger;
+    }
+
+    /// <summary>
+    /// Gets the links.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>An action result containing the links.</returns>
+    [HttpGet("")]
+    public async Task<ActionResult<GetLinksResponse>> GetLinks(CancellationToken cancellationToken)
+    {
+        return new GetLinksResponse { Link = "Link.API" };
+    }
+
+    /// <summary>
+    /// Represents the response for getting links.
+    /// </summary>
+    public class GetLinksResponse
+    {
+        /// <summary>
+        /// Gets or sets the link.
+        /// </summary>
+        public string Link { get; set; }
+    }
+}
