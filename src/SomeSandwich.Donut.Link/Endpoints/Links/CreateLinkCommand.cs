2243bf5abae144cd1b532ca3dfde8947e4664587
Hieu Nguyen <hieucckha@gmail.com>
2024-12-16 14:50:39 +0700

    Add mongodb to project

---
+++ src/SomeSandwich.Donut.Link/Endpoints/Links/CreateLinkCommand.cs
@@ -0,0 +1,21 @@
+﻿using SomeSandwich.Donut.Application.Common.Startup.OpenApi;
+
+namespace SomeSandwich.Donut.Link.Endpoints.Links;
+
+/// <summary>
+/// Represents a command to create a link.
+/// </summary>
+[OpenApiExample<CreateLinkCommand>]
+public class CreateLinkCommand : IExampleProvider<CreateLinkCommand>
+{
+    /// <summary>
+    /// Gets or sets the URL of the link to be created.
+    /// </summary>
+    public required string Url { get; init; }
+
+    /// <inheritdoc />
+    public static CreateLinkCommand GenerateExample()
+    {
+        return new CreateLinkCommand { Url = "https://www.example.com" };
+    }
+}
---
+++ src/SomeSandwich.Donut.Link/Endpoints/Links/CreateLinkEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using SomeSandwich.Donut.Application.Common.Interfaces;
+
+namespace SomeSandwich.Donut.Link.Endpoints.Links;
+
+/// <summary>
+/// Endpoint for creating a new link.
+/// </summary>
+public class CreateLinkEndpoint : IEndpoint
+{
+    /// <inheritdoc />
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("api/link", async (
+            ILogger<CreateLinkCommand> logger,
+            IMongoClient dbClient,
+            [FromBody] CreateLinkCommand command,
+            CancellationToken cancellationToken) =>
+        {
+            var linkDb = dbClient.GetDatabase("LinkDB");
+            var linksCollection = linkDb.GetCollection<Domain.Link.Link>("links");
+
+            await linksCollection.InsertOneAsync(new Domain.Link.Link { Href = command.Url }, null, cancellationToken);
+
+            logger.LogInformation("Create new link with {Url}", command.Url);
+        });
+    }
+}
